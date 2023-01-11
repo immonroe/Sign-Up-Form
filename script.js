739f9150b6aa1d7967f050af973ef376503b8f35
immonroe <immanuelmonroe@gmail.com>
2023-01-11 08:57:00 -0500

    Added notes to both css and js files

---
+++ script.js
@@ -0,0 +1,5 @@
+// The inputs, by default have a very light border (#E5E7EB), but we’ve included 2 variations. For starters, the password inputs should be given an error class.
+
+// The other variation is the selected input, which should have a blue border and subtle box-shadow. This can be done with the :focus pseudo-class you’ve learned about in an earlier lesson.
+
+// Checking that the password fields match each other requires javascript. Try to implement this if you feel confident, otherwise just validate each field separately.
