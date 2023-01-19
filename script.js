b06d8490b6d6d70306e1ebf715bd7aa9abc7327c
immonroe <immanuelmonroe@gmail.com>
2023-01-18 21:31:18 -0500

    Added password matching verfication script

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// Add script that makes sure both passwords match one another
+// Password matching verification
+
+var check = function() {
+    if (document.getElementById('password').value ==
+      document.getElementById('confirm-password').value) {
+      document.getElementById('message').style.color = 'green';
+      document.getElementById('message').innerHTML = 'matching';
+    } else {
+      document.getElementById('message').style.color = 'red';
+      document.getElementById('message').innerHTML = 'not matching';
+    }
+  }
